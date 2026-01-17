e7b0d2c03a273c47b9e58675254de99150ea8f55
Patryk Podolski <107349176+Patras01@users.noreply.github.com>
2026-01-17 23:25:27 +0100

    Update Lab1_029404.py

--- Lab1/Lab1_029404.py
+++ Lab1/Lab1_029404.py
@@ -1,8 +1,7 @@
 def matematyczna_reszta(x, y):
     reszta = x % y
-
     return reszta
 
-
-print(matematyczna_reszta(5, 2))
-print(matematyczna_reszta(-10, 3))
+liczba1 = float(input("Podaj liczba1: "))
+liczba2 = float(input("Podaj liczba2: "))
+print(matematyczna_reszta(liczba1, liczba2))
